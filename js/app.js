b5ce2650728d9a214a28b78b61a1e7eb4c76f4b1
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2018-04-27 15:17:17 +0100

    made site node ready

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-// Material Boxed
-   const mb = document.querySelectorAll('.materialboxed');
-   M.Materialbox.init(mb, {});
+   const express = require('express')
+   const app = express()
+
+   app.get('/', (req, res) => res.send('Hello World!'))
+
+   app.use(express.static('public'));
+
+   app.listen(3000, () => console.log('Example app listening on port 3000!'))
